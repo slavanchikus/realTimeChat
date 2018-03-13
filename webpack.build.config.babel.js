79d6bf8d210bbcbe624ba5f72e01b6cb190d9957
Вячеслав Беляков <beliakov@company.dnevnik.ru>
2018-03-13 16:47:42 +0300

    replace sw and manifest

--- webpack.build.config.babel.js
+++ webpack.build.config.babel.js
@@ -84,20 +84,9 @@ export default () => ({
       fileName: 'asset-manifest.json',
     }),
     new SWPrecacheWebpackPlugin({
-      // By default, a cache-busting query parameter is appended to requests
-      // used to populate the caches, to ensure the responses are fresh.
-      // If a URL is already hashed by Webpack, then there is no concern
-      // about it being stale, and the cache-busting can be skipped.
       dontCacheBustUrlsMatching: /\.\w{8}\./,
       filename: 'service-worker.js',
-      logger(message) {
-        if (message.indexOf('Total precache size is') === 0) {
-          // This message occurs for every build and is a bit too noisy.
-          return;
-        }
-        console.log(message);
-      },
-      minify: true, // minify and uglify the script
+      minify: true,
       navigateFallback: 'build/index.htm',
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
     })
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,5 @@
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import ManifestPlugin from 'webpack-manifest-plugin';
-import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
-
 export default () => ({
   entry: {
     app: ['babel-polyfill', './app/index.js']
@@ -69,27 +66,6 @@ export default () => ({
       allChunks: true,
       disable: true
     }),
-    new ManifestPlugin({
-      fileName: 'asset-manifest.json',
-    }),
-    new SWPrecacheWebpackPlugin({
-      // By default, a cache-busting query parameter is appended to requests
-      // used to populate the caches, to ensure the responses are fresh.
-      // If a URL is already hashed by Webpack, then there is no concern
-      // about it being stale, and the cache-busting can be skipped.
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: 'service-worker.js',
-      logger(message) {
-        if (message.indexOf('Total precache size is') === 0) {
-          // This message occurs for every build and is a bit too noisy.
-          return;
-        }
-        console.log(message);
-      },
-      minify: false, // minify and uglify the script
-      navigateFallback: 'build/index.html',
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-    })
   ],
   devServer: {
     port: 8081,
