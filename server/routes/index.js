6c484342eae68f9ef2039d8fe77c1b31c4d9cbd3
Вячеслав Беляков <beliakov@company.dnevnik.ru>
2018-04-05 16:15:44 +0300

    add audio route for vk music

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,12 @@ const roomsRouters = require('./roomsRoutes');
 const messagesRouters = require('./messagesRoutes');
 const userRoutes = require('./userRoutes');
 const filesRoutes = require('./filesRoutes');
+const audioRoutes = require('./audioRoutes');
 
 module.exports = function(app, db) {
   roomsRouters(app, db);
   messagesRouters(app, db);
   userRoutes(app, db);
   filesRoutes(app, db);
+  audioRoutes(app, db);
 };
